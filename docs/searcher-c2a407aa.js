3c09ea6b125c1dbcdb736588160d37f3b26eb7bc
Tim Martin <timcmartin@gmail.com>
2026-03-27 06:43:29 -0600

    Building book.

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f40b7b8b.js',
+                path_to_root + 'searchindex-cf9379dc.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
