70df284149ecae09a65acb16d81b207fdcdbccbc
Tim Martin <tim.martin@gettyimages.com>
2026-04-11 14:15:34 -0600

    Building book.

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-cf9379dc.js',
+                path_to_root + 'searchindex-60f942d8.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
